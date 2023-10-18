8f831594d527d3321cab074fe303b7bfcf60a4de
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 13:14:20 +0100

    lastOne

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
-// const socket = io.connect("http://localhost:3001");
-const socket = io.connect("https://chat-app-test-phi.vercel.app");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   //Room State
