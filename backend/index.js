fca5488412a52008e1c5c7dad59563db2a885a06
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 18:17:23 +0100

    sc

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.use(cors()); // Enable CORS for all routes
 
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Listen for messages
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // Broadcast the message to all connected clients
+  // Listen for joining a room
+  socket.on('join room', (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
+
+  // Listen for messages in a specific room
+  socket.on('chat message', ({ room, msg }) => {
+    io.to(room).emit('chat message', msg);
   });
 
   // Handle disconnect
--- frontend/src/assets/Chat.jsx
+++ frontend/src/assets/Chat.jsx
@@ -1,3 +1,44 @@
+// import React, { useState, useEffect } from 'react';
+// import io from 'socket.io-client';
+
+// const socket = io('http://localhost:3001'); // Update the URL accordingly
+
+// function Chat() {
+//   const [messages, setMessages] = useState([]);
+//   const [input, setInput] = useState('');
+
+//   useEffect(() => {
+//     // Listen for incoming messages
+//     socket.on('chat message', (msg) => {
+//       setMessages((prevMessages) => [...prevMessages, msg]); // Use a functional update
+//     });
+
+//     return () => {
+//       socket.disconnect();
+//     };
+//   }, []); // Empty dependency array to run this effect only once
+
+//   const sendMessage = () => {
+//     socket.emit('chat message', input);
+//     setInput('');
+//   };
+
+//   return (
+//     <div>
+//       <div style={{ height: '400px', overflowY: 'scroll', border: '1px solid #ccc' }}>
+//         {messages.map((msg, index) => (
+//           <div key={index}>{msg}</div>
+//         ))}
+//       </div>
+//       <div>
+//         <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+//         <button onClick={sendMessage}>Send</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Chat;
 // Chat.js
 
 import React, { useState, useEffect } from 'react';
