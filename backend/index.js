9334f993ed8c45994b217e8fd835103bda063d04
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 14:15:02 +0100

    sxc

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,20 @@ app.get('/home', (req, res) => {
     res.send('Hello');
 });
 
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
-});
+    cors: {
+      origin: "https://chat-app-front-tau.vercel.app", // Update this to your frontend domain
+      methods: ["GET", "POST"],
+    },
+  });
+  
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
-const socket = io.connect("http://localhost:3001");
+// const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-app-test-chi.vercel.app/");
+
 
 function App() {
   //Room State
