ac77841499fe1d9909f63445d8d5fca0e635c3b4
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 14:39:00 +0100

    scscsc

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const app = express();
-const http = require("http");
+// const http = require("http");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
 app.use(cors());
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
+const httpServer = createServer(app);
 
 app.get('/home', (req, res) => {
     res.send('Hello');
 });
 
-// const io = new Server(server, {
+// const io = new Server(httpServer, {
 //   cors: {
 //     origin: "http://localhost:5173",
 //     methods: ["GET", "POST"],
 //   },
 // });
 
-const io = new Server(server, {
+const io = new Server(httpServer, {
     cors: {
-      origin: "https://chat-app-front-tau.vercel.app", // Update this to your frontend domain
+      origin: "http://chat-app-front-tau.vercel.app", // Update this to your frontend domain
       methods: ["GET", "POST"],
     },
   });
@@ -46,7 +48,7 @@ const io = new Server(server, {
   });
   
 
-server.listen(3001, () => {
+  httpServer.listen(3001, () => {
   console.log("SERVER IS RUNNING");
 });
 
