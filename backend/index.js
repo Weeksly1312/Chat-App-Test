8c4f9ee5b071fc692660b7aa73ed1fa7f34b86ff
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 20:25:43 +0100

    test

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const app = express();
-// const http = require("http");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+// const express = require("express");
+// const app = express();
+// // const http = require("http");
+// const { createServer } = require("http");
+// const { Server } = require("socket.io");
+// const cors = require("cors");
 
-app.use(cors());
+// app.use(cors());
 
-// const server = http.createServer(app);
-const httpServer = createServer(app);
+// // const server = http.createServer(app);
+// const httpServer = createServer(app);
 
-app.get("/home", (req, res) => {
-  res.send("Hello");
-});
+// app.get("/home", (req, res) => {
+//   res.send("Hello");
+// });
 
 // const io = new Server(httpServer, {
 //   cors: {
@@ -21,28 +21,73 @@ app.get("/home", (req, res) => {
 //   },
 // });
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: "https://chat-app-front-tau.vercel.app", // Update this to your frontend domain with HTTPS
-    methods: ["GET", "POST"],
-  },
+// // const io = new Server(httpServer, {
+// //   cors: {
+// //     origin: "https://chat-app-front-tau.vercel.app", // Update this to your frontend domain with HTTPS
+// //     methods: ["GET", "POST"],
+// //   },
+// // });
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+
+
+// //   socket.on("disconnect", () => {
+// //     console.log(`User Disconnected: ${socket.id}`);
+// //   });
+// });
+
+// httpServer.listen(3001, () => {
+//   console.log("SERVER IS RUNNING");
+// });
+
+const express = require("express");
+const app = express();
+const { createServer } = require("http");
+const WebSocket = require("ws");
+const cors = require("cors");
+
+app.use(cors());
+
+const httpServer = createServer(app);
+
+app.get("/home", (req, res) => {
+  res.send("Hello");
 });
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+// Create a WebSocket server using the existing HTTP server
+const wsServer = new WebSocket.Server({ server: httpServer });
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
+wsServer.on("connection", (socket) => {
+  console.log(`User Connected: ${socket._socket.remoteAddress}`);
+
+  socket.on("message", (message) => {
+    // Handle incoming messages
+    const data = JSON.parse(message);
+    if (data.type === "join_room") {
+      socket.room = data.room;
+      socket.join(data.room);
+    } else if (data.type === "send_message") {
+      wsServer.clients.forEach((client) => {
+        // Broadcast the message to all clients in the same room
+        if (client.room === socket.room && client !== socket) {
+          client.send(JSON.stringify({ type: "receive_message", ...data }));
+        }
+      });
+    }
   });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+  socket.on("close", () => {
+    console.log(`User Disconnected: ${socket._socket.remoteAddress}`);
   });
-
-
-//   socket.on("disconnect", () => {
-//     console.log(`User Disconnected: ${socket.id}`);
-//   });
 });
 
 httpServer.listen(3001, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,63 @@
-import io from "socket.io-client";
+// import io from "socket.io-client";
+// import { useEffect, useState } from "react";
+
+// // const socket = io.connect("http://localhost:3001");
+// // const socket = io.connect("https://chat-app-test-chi.vercel.app/");
+// const socket = new WebSocket('ws://some-websocket-website.com');
+
+// function App() {
+//   //Room State
+//   const [room, setRoom] = useState("");
+
+//   // Messages States
+//   const [message, setMessage] = useState("");
+//   const [messageReceived, setMessageReceived] = useState("");
+
+//   const joinRoom = () => {
+//     if (room !== "") {
+//       socket.emit("join_room", room);
+//     }
+//   };
+
+//   const sendMessage = () => {
+//     socket.emit("send_message", { message, room });
+//   };
+
+//   useEffect(() => {
+//     socket.on("receive_message", (data) => {
+//       setMessageReceived(data.message);
+//     });
+//   }, []);
+//   return (
+//     <div className="App">
+//       <input
+//         placeholder="Room Number..."
+//         onChange={(event) => {
+//           setRoom(event.target.value);
+//         }}
+//       />
+//       <button onClick={joinRoom}> Join Room</button>
+//       <input
+//         placeholder="Message..."
+//         onChange={(event) => {
+//           setMessage(event.target.value);
+//         }}
+//       />
+//       <button onClick={sendMessage}> Send Message</button>
+//       <h1> Message:</h1>
+//       {messageReceived}
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useEffect, useState } from "react";
 
-// const socket = io.connect("http://localhost:3001");
-const socket = io.connect("https://chat-app-test-chi.vercel.app/");
-
+const socket = new WebSocket("ws://some-websocket-website.com");
 
 function App() {
-  //Room State
+  // Room State
   const [room, setRoom] = useState("");
 
   // Messages States
@@ -15,19 +66,34 @@ function App() {
 
   const joinRoom = () => {
     if (room !== "") {
-      socket.emit("join_room", room);
+      // Use JSON.stringify to send data as a JSON string
+      socket.send(JSON.stringify({ type: "join_room", room }));
     }
   };
 
   const sendMessage = () => {
-    socket.emit("send_message", { message, room });
+    // Use JSON.stringify to send data as a JSON string
+    socket.send(JSON.stringify({ type: "send_message", message, room }));
   };
 
   useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-    });
+    // Handle incoming messages
+    const handleIncomingMessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === "receive_message") {
+        setMessageReceived(data.message);
+      }
+    };
+
+    // Add the event listener
+    socket.addEventListener("message", handleIncomingMessage);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      socket.removeEventListener("message", handleIncomingMessage);
+    };
   }, []);
+
   return (
     <div className="App">
       <input
