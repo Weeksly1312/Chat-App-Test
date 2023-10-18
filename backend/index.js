91ef3c5542066122ed382f27127939ee51c79cdc
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 14:10:11 +0100

    sx

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,37 @@
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-// const { Server } = require("socket.io");
-// const cors = require("cors");
-
-// app.use(cors());
-
-// const server = http.createServer(app);
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// io.on("connection", (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
-
-//   socket.on("join_room", (data) => {
-//     socket.join(data);
-//   });
-
-//   socket.on("send_message", (data) => {
-//     socket.to(data.room).emit("receive_message", data);
-//   });
-// });
-
-// server.listen(3001, () => {
-//   console.log("SERVER IS RUNNING");
-// });
-
-// app.js
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-// Define a route that displays "Hello"
+app.use(cors());
+
+const server = http.createServer(app);
+
 app.get('/home', (req, res) => {
     res.send('Hello');
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
+
