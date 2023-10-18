2791745c0c6c70322869b4162a49dd860640c7ee
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 13:17:44 +0100

    sc

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,48 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const cors = require("cors");
+
+// app.use(cors());
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log("SERVER IS RUNNING");
+// });
+
+// app.js
+
+const express = require('express');
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const port = 3000;
 
-app.use(cors());
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
+// Define a route that displays "Hello"
+app.get('/', (req, res) => {
+    res.send('Hello');
 });
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-});
-
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING");
+// Start the server
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
