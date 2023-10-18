09d044d8574eec29d8740820f3fe49dd382f8075
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 18:17:18 +0100

    scsc

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import Chat from './Chat';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const socket = io('http://localhost:3001'); // Update the URL accordingly
+
+function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    // Listen for incoming messages
+    socket.on('chat message', (msg) => {
+      setMessages([...messages, msg]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [messages]);
+
+  const sendMessage = () => {
+    socket.emit('chat message', input);
+    setInput('');
+  };
+
   return (
     <div>
-      <h1>Simple Chat App</h1>
-      <Chat />
+      <div style={{ height: '400px', overflowY: 'scroll', border: '1px solid #ccc' }}>
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
+      <div>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Chat;
