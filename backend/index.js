c05db8c9f2a4af0f1a3ac014ed30741a8d274fdd
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 17:49:39 +0100

    ax

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,28 @@
-// backend/index.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-const express = require("express");
 const app = express();
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const server = http.createServer(app);
+const io = socketIo(server);
 
-app.use(cors());
+app.use(express.static('public'));
 
-const httpServer = createServer(app);
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: "https://chat-app-front-tau.vercel.app", // Fix the typo here
-    methods: ["GET", "POST"],
-  },
-  path: "/socket.io",
-});
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
+  // Listen for messages
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
   });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`User Disconnected: ${socket.id}`);
+  // Handle disconnect
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
   });
 });
 
 const PORT = process.env.PORT || 3001;
-httpServer.listen(PORT, () => {
-  console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
