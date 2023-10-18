20d122a0e8fa31c752cc153e6e159e7d931fb375
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-18 14:08:51 +0100

    c

--- backend/index.js
+++ backend/index.js
@@ -36,11 +36,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// Add this middleware before your routes
-app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 // Define a route that displays "Hello"
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.send('Hello');
 });
 
